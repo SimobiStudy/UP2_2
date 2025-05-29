3e5131bdd6aad795bcff825a9663f5017fd63927
SimobiSirOP <simobisirop@gmail.com>
2025-05-29 11:21:34 +0300

    Buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,15 @@
         {
             textBox1 = new TextBox();
             label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // textBox1
             // 
             textBox1.BackColor = Color.White;
-            textBox1.Location = new Point(175, 37);
+            textBox1.Location = new Point(54, 61);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(361, 27);
+            textBox1.Size = new Size(281, 27);
             textBox1.TabIndex = 0;
             textBox1.LostFocus += textbox1_LostFocus;
             // 
@@ -45,19 +46,30 @@
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.White;
-            label1.Location = new Point(66, 40);
+            label1.Location = new Point(22, 28);
             label1.Name = "label1";
             label1.Size = new Size(103, 20);
             label1.TabIndex = 1;
             label1.Text = "Введите текст";
             label1.Click += label1_Click;
             // 
+            // button1
+            // 
+            button1.Location = new Point(81, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(205, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Очистить текст";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.AntiqueWhite;
-            ClientSize = new Size(595, 533);
+            ClientSize = new Size(673, 249);
+            Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Name = "Form1";
@@ -71,5 +83,6 @@
 
         private TextBox textBox1;
         private Label label1;
+        private Button button1;
     }
 }
